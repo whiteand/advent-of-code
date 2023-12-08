0ea3d87dc45b7e6fe8599ec6195f1b8ee490d71a
whiteand <andrewbeletskiy@gmail.com>
2023-12-08 19:09:14 +0200

    day08: refactored

--- src/y23/y23d08.rs
+++ src/y23/y23d08.rs
@@ -26,18 +26,9 @@ impl Deref for Position {
     type Target = usize;
 }
 
-impl Into<usize> for Position {
-    fn into(self) -> usize {
-        self.0
-    }
-}
-
 impl Position {
     const START: Self = Self(0);
     const END: Self = Self(25 * 26 * 26 + 25 * 26 + 25);
-    fn new() -> Self {
-        Self::START
-    }
     fn turn(&mut self, direction: Direction, network: &[(Position, Position)]) {
         self.0 = match direction {
             Direction::Left => network[self.0].0 .0,
@@ -134,78 +125,10 @@ pub fn solve_task1(file_content: &str) -> usize {
         + 1
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct Ending {
-    cycle: usize,
-    ending_position: Position,
-    turn_index: usize,
-    id: usize,
-}
 #[derive(Debug)]
 struct PositionInfo {
     loop_start_cycle: usize,
     loop_reset_cycle: usize,
-    endings: Vec<Ending>,
-}
-
-struct EndingIterator {
-    endings: Vec<Ending>,
-    loop_start_ending_index: usize,
-    cycles_per_loop: usize,
-    index: usize,
-    loops: usize,
-    cycle: usize,
-    turn_index: usize,
-}
-
-impl EndingIterator {
-    fn time(&self, turns_per_loop: usize) -> usize {
-        self.cycle * turns_per_loop + self.turn_index
-    }
-}
-impl Iterator for EndingIterator {
-    type Item = Ending;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let ind = self.index;
-
-        let mut ending = self.endings[ind].clone();
-        ending.cycle += self.loops * self.cycles_per_loop;
-
-        self.index += 1;
-        if self.index >= self.endings.len() {
-            self.loops += 1;
-            self.index = self.loop_start_ending_index;
-        };
-
-        self.cycle = ending.cycle;
-        self.turn_index = ending.turn_index;
-
-        Some(ending)
-    }
-}
-
-impl IntoIterator for PositionInfo {
-    type Item = Ending;
-    type IntoIter = EndingIterator;
-
-    fn into_iter(self) -> Self::IntoIter {
-        let loop_start_ending_index = self
-            .endings
-            .iter()
-            .position(|e| e.cycle >= self.loop_start_cycle)
-            .unwrap();
-
-        EndingIterator {
-            endings: self.endings,
-            cycles_per_loop: self.loop_reset_cycle - self.loop_start_cycle,
-            index: 0,
-            loops: 0,
-            cycle: 0,
-            turn_index: 0,
-            loop_start_ending_index,
-        }
-    }
 }
 
 fn get_position_info(
@@ -216,20 +139,11 @@ fn get_position_info(
     let mut pos = start;
     let mut cycle = 0;
     let mut cycle_finish_positions = vec![start];
-    let mut endings = Vec::new();
     let loop_start_cycle;
     let loop_reset_cycle;
     loop {
-        for (i, &t) in turns.iter().enumerate() {
+        for &t in turns.iter() {
             pos.turn(t, network);
-            if pos.0 % 26 == 25 {
-                endings.push(Ending {
-                    id: endings.len(),
-                    cycle,
-                    ending_position: pos,
-                    turn_index: i,
-                });
-            }
         }
         cycle += 1;
         if let Some(i) = cycle_finish_positions.iter().position(|&p| p == pos) {
@@ -243,62 +157,41 @@ fn get_position_info(
     PositionInfo {
         loop_reset_cycle,
         loop_start_cycle,
-        endings,
     }
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    let mut a = a;
+    let mut b = b;
+    while a > 0 && b > 0 {
+        if a > b {
+            a %= b;
+        } else {
+            b %= a
+        }
+    }
+    a.max(b)
+}
+fn nok(a: usize, b: usize) -> usize {
+    a / gcd(a, b) * b
 }
 
 pub fn solve_task2(file_content: &str) -> usize {
     let (turns, nodes) = parse_input(file_content);
     let turns_per_cycle = turns.len();
-    println!("Turns: {} ", turns_per_cycle);
 
     let mut network = vec![(Position::START, Position::START); 26 * 26 * 26];
     for node in &nodes {
         network[node.0 .0] = (node.1, node.2);
     }
 
-    let position_infos = nodes
+    nodes
         .into_iter()
         .map(|(n, _, _)| n)
         .filter(|n| n.0 % 26 == 0)
         .map(|p| get_position_info(&turns, &network, p))
-        .collect::<Vec<_>>();
-
-    let mut carets = position_infos
-        .into_iter()
-        .map(|p| p.into_iter())
-        .collect::<Vec<_>>();
-
-    for c in &mut carets {
-        c.next().unwrap();
-    }
-
-    let mut i = 0;
-
-    loop {
-        let mut min_time = usize::MAX;
-        let mut max_time = 0;
-        let mut min_time_index = 0;
-        for (i, c) in carets.iter().enumerate() {
-            let t = c.time(turns_per_cycle);
-            if t < min_time {
-                min_time = t;
-                min_time_index = i;
-            }
-            if t > max_time {
-                max_time = t
-            }
-        }
-        i += 1;
-        if i % 10000 == 0 {
-            println!("{}: {}", i, max_time - min_time);
-        }
-        if min_time == max_time {
-            return min_time + 1;
-        } else {
-            carets[min_time_index].next().unwrap();
-        }
-    }
+        .map(|p| (p.loop_reset_cycle - p.loop_start_cycle) * turns_per_cycle)
+        .fold(1usize, nok)
 }
 #[cfg(test)]
 mod tests {
@@ -354,6 +247,6 @@ XXX = (XXX, XXX)"
 
     #[test]
     fn test_task2_actual() {
-        assert_eq!(format!("{}", solve_task2(ACTUAL)), "0");
+        assert_eq!(format!("{}", solve_task2(ACTUAL)), "15726453850399");
     }
 }
