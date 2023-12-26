fb920c77a674cbf510331307f314b978bad401da
whiteand <andrewbeletskiy@gmail.com>
2023-12-26 15:24:21 +0200

    day21: refactored part 1

--- y23/d21/src/lib.rs
+++ y23/d21/src/lib.rs
@@ -1,8 +1,34 @@
-use std::collections::VecDeque;
+use std::collections::{BTreeMap, VecDeque};
+
+trait Field {
+    fn is_empty(&self, coord: (isize, isize)) -> bool;
+    fn get_start(&self) -> (isize, isize);
+}
 
 struct Grid {
     cells: Vec<Vec<bool>>,
     start: (usize, usize),
+}
+
+impl Field for Grid {
+    fn is_empty(&self, coord: (isize, isize)) -> bool {
+        if coord.0 < 0 || coord.1 < 0 {
+            return false;
+        }
+        let row = coord.0 as usize;
+        let col = coord.1 as usize;
+        if row >= self.cells.len() {
+            return false;
+        }
+        if col >= self.cells[0].len() {
+            return false;
+        }
+        !self.cells[row][col]
+    }
+
+    fn get_start(&self) -> (isize, isize) {
+        (self.start.0 as isize, self.start.1 as isize)
+    }
 }
 
 impl std::fmt::Debug for Grid {
@@ -16,6 +42,53 @@ impl std::fmt::Debug for Grid {
                     write!(f, "#")?;
                 } else {
                     write!(f, ".")?;
+                }
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
+
+struct Infinite(Grid);
+
+impl Infinite {
+    fn get_row(&self, row: isize) -> usize {
+        let rows = self.0.cells.len();
+        row.rem_euclid(rows as isize) as usize
+    }
+    fn get_col(&self, col: isize) -> usize {
+        let cols = self.0.cells[0].len();
+        col.rem_euclid(cols as isize) as usize
+    }
+    fn get_coords(&self, coord: (isize, isize)) -> (usize, usize) {
+        (self.get_row(coord.0), self.get_col(coord.1))
+    }
+}
+
+impl Field for Infinite {
+    fn is_empty(&self, coord: (isize, isize)) -> bool {
+        let (r, c) = self.get_coords(coord);
+        self.0.is_empty((r as isize, c as isize))
+    }
+    fn get_start(&self) -> (isize, isize) {
+        self.0.get_start()
+    }
+}
+
+impl std::fmt::Debug for Infinite {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let min_r = -(self.0.cells.len() as isize);
+        let max_r = (self.0.cells.len() * 2) as isize;
+        let min_c = -(self.0.cells[0].len() as isize);
+        let max_c = (self.0.cells[0].len() * 2) as isize;
+
+        for i in min_r..=max_r {
+            for j in min_c..=max_c {
+                if self.is_empty((i, j)) {
+                    write!(f, ".")?;
+                } else {
+                    write!(f, "#")?;
                 }
             }
             writeln!(f)?;
@@ -45,32 +118,79 @@ fn parse_grid(input: &str) -> Grid {
     Grid { cells: grid, start }
 }
 
-fn get_min_distances(grid: &Grid) -> Vec<Vec<u16>> {
-    let mut min_distances = vec![vec![u16::MAX; grid.cells[0].len()]; grid.cells.len()];
+struct Counter {
+    total: usize,
+    current_distance: usize,
+    current_count: usize,
+    max_distance: usize,
+}
+
+impl Counter {
+    fn new(max_distance: usize) -> Self {
+        Self {
+            current_distance: 0,
+            total: 0,
+            current_count: 0,
+            max_distance,
+        }
+    }
+    fn push(&mut self, distance: usize) {
+        if self.current_distance == distance {
+            self.current_count += 1;
+            return;
+        }
+
+        if self.current_distance % 2 == self.max_distance % 2 {
+            self.total += self.current_count;
+            self.current_count = 1;
+            self.current_distance = distance;
+            return;
+        }
+        self.current_count = 1;
+        self.current_distance = distance;
+    }
+
+    fn into_total(self) -> usize {
+        if self.current_distance % 2 == self.max_distance % 2 {
+            self.total + self.current_count
+        } else {
+            self.total
+        }
+    }
+}
+
+fn get_min_distances<T: Field>(grid: &T, max_d: usize) -> usize {
+    let mut counter = Counter::new(max_d);
+    let mut min_distances = BTreeMap::new();
 
     let mut queue = VecDeque::new();
-    queue.push_back((grid.start, 0));
+    queue.push_back((grid.get_start(), 0));
 
     while let Some((coord, dist)) = queue.pop_front() {
-        if min_distances[coord.0][coord.1] <= dist {
+        if min_distances.contains_key(&coord) {
             continue;
         }
-        min_distances[coord.0][coord.1] = dist;
-        if coord.0 > 0 && !grid.cells[coord.0 - 1][coord.1] {
-            queue.push_back(((coord.0 - 1, coord.1), dist + 1));
+        if dist > max_d {
+            break;
         }
-        if coord.0 < grid.cells.len() - 1 && !grid.cells[coord.0 + 1][coord.1] {
-            queue.push_back(((coord.0 + 1, coord.1), dist + 1));
+        counter.push(dist);
+        min_distances.insert(coord, dist);
+        let (r, c) = coord;
+        if grid.is_empty((r - 1, c)) && !min_distances.contains_key(&(r - 1, c)) {
+            queue.push_back(((r - 1, c), dist + 1));
         }
-        if coord.1 > 0 && !grid.cells[coord.0][coord.1 - 1] {
-            queue.push_back(((coord.0, coord.1 - 1), dist + 1));
+        if grid.is_empty((r + 1, c)) && !min_distances.contains_key(&(r + 1, c)) {
+            queue.push_back(((r + 1, c), dist + 1));
         }
-        if coord.1 < grid.cells[0].len() - 1 && !grid.cells[coord.0][coord.1 + 1] {
-            queue.push_back(((coord.0, coord.1 + 1), dist + 1));
+        if grid.is_empty((r, c - 1)) && !min_distances.contains_key(&(r, c - 1)) {
+            queue.push_back(((r, c - 1), dist + 1));
+        }
+        if grid.is_empty((r, c + 1)) && !min_distances.contains_key(&(r, c + 1)) {
+            queue.push_back(((r, c + 1), dist + 1));
         }
     }
 
-    min_distances
+    counter.into_total()
 }
 
 fn print_distances(ds: &[Vec<u16>]) {
@@ -86,57 +206,54 @@ fn print_distances(ds: &[Vec<u16>]) {
     }
 }
 
-fn solve(file_content: &str, steps: usize) -> usize {
+fn solve_finite(file_content: &str, steps: usize) -> usize {
     let grid = parse_grid(file_content);
-    let min_distance = get_min_distances(&grid);
-    let max_distance = min_distance
-        .iter()
-        .flatten()
-        .copied()
-        .filter(|x| *x != u16::MAX)
-        .max()
-        .unwrap();
-    let mut counts_per_distance: Vec<u16> = Vec::new();
-    println!("{:?}", grid);
-    print_distances(&min_distance);
-    for i in 0..=max_distance {
-        let count = min_distance.iter().flatten().filter(|x| **x == i).count();
-        counts_per_distance.push(count as u16);
-        println!("{}: {}", i, count);
-    }
-    (0..=max_distance)
-        .filter(|x| (*x as usize) <= steps)
-        .filter(|x| (*x as usize) % 2 == steps % 2)
-        .map(|x| counts_per_distance[x as usize] as usize)
-        .sum()
+    get_min_distances(&grid, steps)
+}
+fn solve_infinite(file_content: &str, steps: usize) -> usize {
+    todo!("Not implemented yet");
+    let grid = parse_grid(file_content);
+    let grid = Infinite(grid);
+    get_min_distances(&grid, steps)
 }
 
 pub fn solve_part_1(file_content: &str) -> usize {
-    solve(file_content, 64)
+    solve_finite(file_content, 64)
 }
-pub fn solve_part_2(_file_content: &str) -> usize {
-    todo!("part 2 is not implemented yet")
+pub fn solve_part_2(file_content: &str) -> usize {
+    solve_infinite(file_content, 26501365)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{solve, solve_part_2};
+    use rstest::rstest;
+
+    use crate::solve_infinite;
+
+    use super::{solve_finite, solve_part_2};
     const EXAMPLE: &str = include_str!("../example.txt");
     const ACTUAL: &str = include_str!("../input.txt");
     #[test]
     fn test_part1() {
-        assert_eq!(format!("{}", solve(EXAMPLE, 6)), "16");
+        assert_eq!(format!("{}", solve_finite(EXAMPLE, 6)), "16");
     }
 
     #[test]
     fn test_part1_actual() {
-        assert_eq!(format!("{}", solve(ACTUAL, 64)), "3740");
+        assert_eq!(format!("{}", solve_finite(ACTUAL, 64)), "3740");
     }
 
-    #[test]
+    #[rstest]
+    #[case(6, 16)]
+    #[case(10, 50)]
+    #[case(50, 1594)]
+    #[case(100, 6536)]
+    #[case(500, 167004)]
+    #[case(1000, 668697)]
+    #[case(5000, 16733044)]
     #[ignore]
-    fn test_part2() {
-        assert_eq!(format!("{}", solve_part_2(EXAMPLE)), "0");
+    fn test_part2(#[case] steps: usize, #[case] expected: usize) {
+        assert_eq!(solve_infinite(EXAMPLE, steps), expected);
     }
 
     #[test]
