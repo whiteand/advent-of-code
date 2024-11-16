fca3b2db7795467beaf6df7594c2fd9d8fd52ea2
whiteand <andrewbeletskiy@gmail.com>
2024-11-16 23:53:41 +0200

    feat: added solution

--- y15/d19/src/lib.rs
+++ y15/d19/src/lib.rs
@@ -1,129 +1,83 @@
 mod trie;
-use std::collections::{BTreeMap, BTreeSet, BinaryHeap};
 
 use itertools::Itertools;
+use trie::Trie;
 
 pub fn solve_part_1(file_content: &str) -> usize {
     let (replacements, molecule) = parse_input(file_content);
-    println!("{replacements:?}\n{molecule}");
-    let inputs = replacements.keys().copied().collect_vec();
-    modify(&replacements, &inputs, molecule).len()
+    let mut output = Vec::with_capacity(molecule.len());
+    modify(&replacements, molecule.trim(), &mut output);
+    output.len()
 }
 
-fn modify(
-    replacements: &BTreeMap<&str, Vec<&str>>,
-    inputs: &[&str],
-    molecule: &str,
-) -> Vec<String> {
+fn modify(replacements: &[(&str, Vec<&str>)], molecule: &str, output: &mut Vec<String>) {
     let mut buf = String::with_capacity(molecule.len());
-    let mut res: BTreeSet<String> = BTreeSet::new();
+    let mut visited = Trie::new();
 
-    for i in 0..molecule.as_bytes().len() {
-        for inp in inputs {
-            if molecule.as_bytes()[i..].starts_with(inp.as_bytes()) {
-                let options = replacements.get(inp).unwrap();
-                for option in options.iter() {
-                    buf.push_str(molecule.get(0..i).unwrap_or_default());
-                    buf.push_str(option);
-                    buf.push_str(molecule.get((i + inp.len())..).unwrap_or_default());
-                    res.insert(buf.clone());
-                    buf.clear();
+    for (inp, options) in replacements {
+        let mut next_ind = 0;
+        while let Some(ind) = molecule.get(next_ind..).unwrap_or_default().find(inp) {
+            let real_index = ind + next_ind;
+            for option in options {
+                buf.push_str(molecule.get(0..real_index).unwrap_or_default());
+                buf.push_str(option);
+                buf.push_str(molecule.get((real_index + inp.len())..).unwrap_or_default());
+                if !visited.contains(&buf) {
+                    visited.push(&buf);
+                    output.push(buf.clone())
+                }
+                buf.clear();
+            }
+            next_ind = real_index + 1;
+        }
+    }
+}
+pub fn solve_part_2(file_content: &str) -> usize {
+    let (replacements, molecule) = parse_input(file_content);
+    let mut molecule = molecule.to_owned();
+    let mut res = 0;
+    loop {
+        let mut done = true;
+        for (input, outputs) in replacements.iter() {
+            for output in outputs {
+                if let Some(pos) = molecule.find(output) {
+                    let mut new_string = String::new();
+                    new_string.push_str(molecule.get(0..pos).unwrap_or_default());
+                    new_string.push_str(input);
+                    new_string.push_str(molecule.get((pos + output.len())..).unwrap_or_default());
+                    molecule = new_string;
+                    res += 1;
+                    done = false;
                 }
             }
         }
-    }
-
-    res.into_iter().collect_vec()
-}
-pub fn solve_part_2(file_content: &str) -> usize {
-    let (replacements, molecule) = parse_reverse_input(file_content);
-    let mut tasks = BinaryHeap::new();
-    let mut inputs = replacements.keys().copied().collect_vec();
-    inputs.sort_by_key(|a| a.len());
-    inputs.reverse();
-    println!("{inputs:?}");
-    let max_diff = replacements
-        .iter()
-        .flat_map(|(prev, nexts)| nexts.iter().map(|next| prev.len() - next.len()))
-        .max()
-        .unwrap_or(usize::MAX);
-    tasks.push(Task::new(0, molecule.trim().to_owned(), max_diff));
-    let mut visited = trie::Trie::new();
-    while let Some(task) = tasks.pop() {
-        if tasks.len() % 1000 == 0 {
-            println!("{} => {} | rem: {}", task.score, task.str, tasks.len());
-        }
-        if &task.str == "e" {
-            return task.steps;
-        }
-        let next_iterations = modify(&replacements, &inputs, &task.str);
-        visited.push(&task.str);
-        for it in next_iterations {
-            if visited.contains(&it) {
-                continue;
-            }
-            tasks.push(Task::new(task.steps + 1, it, max_diff));
+        if done {
+            break;
         }
     }
-    0
+    res
 }
 
-fn parse_input(file_content: &str) -> (BTreeMap<&str, Vec<&str>>, &str) {
+fn parse_input(file_content: &str) -> (Vec<(&str, Vec<&str>)>, &str) {
     let (replacements_input, molecule) = file_content.split_once("\n\n").unwrap();
-    let replacements = replacements_input
+
+    let mut replacements = replacements_input
         .lines()
         .map(|line| {
             let (left, right) = line.split_once(" => ").unwrap();
             (left, right)
         })
-        .fold(
-            BTreeMap::new(),
-            |mut map: BTreeMap<&str, Vec<&str>>, (left, right)| {
-                map.entry(left).or_default().push(right);
-                map
-            },
-        );
+        .into_group_map_by(|x| x.0)
+        .into_iter()
+        .map(|(a, b)| (a, b.into_iter().map(|x| x.1).collect_vec()))
+        .collect_vec();
 
-    (replacements, molecule)
-}
-
-#[derive(Eq, PartialEq, Debug)]
-struct Task {
-    str: String,
-    steps: usize,
-    score: usize,
-}
-impl Task {
-    fn new(steps: usize, str: String, max_diff: usize) -> Self {
-        let score = steps * max_diff + (str.len() - 1);
-        Self { str, steps, score }
+    for (_, nexts) in replacements.iter_mut() {
+        nexts.sort_by_key(|x| x.len());
+        nexts.reverse();
     }
-}
-impl std::cmp::PartialOrd for Task {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-impl std::cmp::Ord for Task {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        other.score.cmp(&self.score)
-    }
-}
-fn parse_reverse_input(file_content: &str) -> (BTreeMap<&str, Vec<&str>>, &str) {
-    let (replacements_input, molecule) = file_content.split_once("\n\n").unwrap();
-    let replacements = replacements_input
-        .lines()
-        .map(|line| {
-            let (left, right) = line.split_once(" => ").unwrap();
-            (right, left)
-        })
-        .fold(
-            BTreeMap::new(),
-            |mut map: BTreeMap<&str, Vec<&str>>, (left, right)| {
-                map.entry(left).or_default().push(right);
-                map
-            },
-        );
+    replacements.sort_by_key(|x| x.0.len());
+    replacements.reverse();
 
     (replacements, molecule)
 }
