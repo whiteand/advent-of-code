39cec301918ebdf57cd329c29659e4a6f4b68de6
whiteand <andrewbeletskiy@gmail.com>
2024-12-07 10:28:16 +0200

    feat: added macros

--- y24/d07/src/lib.rs
+++ y24/d07/src/lib.rs
@@ -1,45 +1,16 @@
+use std::marker::PhantomData;
+
 #[tracing::instrument(skip(file_content))]
 pub fn solve_part_1(file_content: &str) -> u128 {
-    solve::<AddMul>(file_content)
+    solve::<Or<Mul, Add>>(file_content)
 }
 
 #[tracing::instrument(skip(file_content))]
 pub fn solve_part_2(file_content: &str) -> u128 {
-    solve::<AddMulConcat>(file_content)
+    solve::<Or<Concat, Or<Mul, Add>>>(file_content)
 }
 
-struct AddMul;
-impl Operations for AddMul {
-    fn reverse(result: u128, op: u128) -> impl Iterator<Item = u128> {
-        let mut state = 0;
-        std::iter::from_fn(move || match state {
-            0 => {
-                state += 1;
-                (result % op == 0).then(|| result / op)
-            }
-            1 => {
-                state += 1;
-                (result >= op).then(|| result - op)
-            }
-            _ => None,
-        })
-    }
-}
-struct AddMulConcat;
-
-impl Operations for AddMulConcat {
-    fn reverse(result: u128, op: u128) -> impl Iterator<Item = u128> {
-        std::iter::from_fn(move || trim_suffix(result, op))
-            .take(1)
-            .chain(AddMul::reverse(result, op))
-    }
-}
-
-trait Operations {
-    fn reverse(result: u128, op: u128) -> impl Iterator<Item = u128>;
-}
-
-fn solve<Ops: Operations>(file_content: &str) -> u128 {
+fn solve<Ops: Operation>(file_content: &str) -> u128 {
     let mut buf = Vec::with_capacity(20);
     file_content
         .lines()
@@ -56,7 +27,8 @@ fn solve<Ops: Operations>(file_content: &str) -> u128 {
         .sum()
 }
 
-fn can_be_constructed<Ops: Operations>(result: u128, operands: &[u128]) -> bool {
+// #[tracing::instrument(ret)]
+fn can_be_constructed<Ops: Operation>(result: u128, operands: &[u128]) -> bool {
     let Some((last, rest)) = operands.split_last() else {
         return false;
     };
@@ -66,6 +38,31 @@ fn can_be_constructed<Ops: Operations>(result: u128, operands: &[u128]) -> bool 
     } else {
         Ops::reverse(result, last).any(|prev_result| can_be_constructed::<Ops>(prev_result, rest))
     }
+}
+
+macro_rules! impl_op {
+    ($id:ident, $res:ident, $op:ident, $expr:expr) => {
+        struct $id;
+        impl Operation for $id {
+            fn reverse($res: u128, $op: u128) -> impl Iterator<Item = u128> {
+                std::iter::from_fn(move || $expr).take(1)
+            }
+        }
+    };
+}
+impl_op!(Mul, result, op, (result % op == 0).then(|| result / op));
+impl_op!(Add, result, op, (result >= op).then(|| result - op));
+impl_op!(Concat, result, op, trim_suffix(result, op));
+
+struct Or<A, B>(PhantomData<(A, B)>);
+impl<A: Operation, B: Operation> Operation for Or<A, B> {
+    fn reverse(result: u128, op: u128) -> impl Iterator<Item = u128> {
+        A::reverse(result, op).chain(B::reverse(result, op))
+    }
+}
+
+trait Operation {
+    fn reverse(result: u128, op: u128) -> impl Iterator<Item = u128>;
 }
 
 fn trim_suffix(mut long: u128, mut short: u128) -> Option<u128> {
