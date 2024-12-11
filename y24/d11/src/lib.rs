821c7c770aff5fa60fefc6167d588f93a7a3dae2
whiteand <andrewbeletskiy@gmail.com>
2024-12-11 08:19:58 +0200

    feat: improved performance

--- y24/d11/src/lib.rs
+++ y24/d11/src/lib.rs
@@ -1,6 +1,6 @@
 use core::str;
 use itertools::Itertools;
-use rayon::prelude::*;
+use std::collections::HashMap;
 
 #[tracing::instrument(skip(file_content))]
 pub fn solve_part_1(file_content: &str) -> usize {
@@ -12,63 +12,57 @@ pub fn solve(file_content: &str, blinks: usize) -> usize {
         .trim()
         .split_ascii_whitespace()
         .map(|x| x.parse::<usize>().unwrap())
-        .collect_vec();
-    let mut buf = Vec::with_capacity(nums.len());
-    for i in 0..blinks {
-        tracing::info!(n = nums.len(), i, "blink");
-        blink(&mut nums, &mut buf);
+        .counts();
+    let mut buf = HashMap::new();
+
+    for _ in 0..blinks {
+        blink(&nums, &mut buf);
         (nums, buf) = (buf, nums);
     }
-    tracing::info!(b = buf.len(), s = nums.len());
 
     if blinks % 2 == 0 {
-        buf.len()
+        buf.values().sum()
     } else {
-        nums.len()
+        nums.values().sum()
     }
 }
-fn blink(src: &mut Vec<usize>, dst: &mut Vec<usize>) {
+fn blink(src: &HashMap<usize, usize>, dst: &mut HashMap<usize, usize>) {
     dst.clear();
-    dst.par_extend(src.par_iter().flat_map_iter(|stone| {
+    for (stone, n) in src {
         let stone = *stone;
         if stone == 0 {
-            [1, 0].into_iter().take(1)
+            *dst.entry(1).or_insert(0) += n;
         } else if let Some((left, right)) = split_even(stone) {
-            [left, right].into_iter().take(2)
+            *dst.entry(left).or_insert(0) += n;
+            *dst.entry(right).or_insert(0) += n;
         } else {
-            [stone * 2024, 0].into_iter().take(1)
+            *dst.entry(stone * 2024).or_insert(0) += n;
         }
-    }));
+    }
 }
 
 fn split_even(n: usize) -> Option<(usize, usize)> {
-    let digits = count_digits(n);
-    if digits % 2 == 0 {
-        Some(split_at(n, digits / 2))
-    } else {
-        None
+    match n {
+        0..10 => None,
+        10..100 => Some((n / 10, n % 10)),
+        100..1000 => None,
+        1000..10000 => Some((n / 100, n % 100)),
+        10000..100000 => None,
+        100000..1000000 => Some((n / 1000, n % 1000)),
+        1000000..10000000 => None,
+        10000000..100000000 => Some((n / 10000, n % 10000)),
+        100000000..1000000000 => None,
+        1000000000..10000000000 => Some((n / 100000, n % 100000)),
+        10000000000..100000000000 => None,
+        100000000000..1000000000000 => Some((n / 1000000, n % 1000000)),
+        1000000000000..10000000000000 => None,
+        10000000000000..100000000000000 => Some((n / 10000000, n % 10000000)),
+        100000000000000..1000000000000000 => None,
+        1000000000000000..10000000000000000 => Some((n / 100000000, n % 100000000)),
+        10000000000000000..100000000000000000 => None,
+        100000000000000000..1000000000000000000 => Some((n / 1000000000, n % 1000000000)),
+        x => unreachable!("{x}"),
     }
-}
-fn split_at(mut n: usize, k: usize) -> (usize, usize) {
-    let mut reversed_right = 0;
-    for _ in 0..k {
-        reversed_right *= 10;
-        reversed_right += n % 10;
-        n /= 10;
-    }
-    let mut right = 0;
-    for _ in 0..k {
-        right *= 10;
-        right += reversed_right % 10;
-        reversed_right /= 10;
-    }
-    (n, right)
-}
-fn count_digits(n: usize) -> usize {
-    if n < 10 {
-        return 1;
-    }
-    return 1 + count_digits(n / 10);
 }
 
 #[tracing::instrument(skip(file_content))]
@@ -78,25 +72,17 @@ pub fn solve_part_2(file_content: &str) -> usize {
 
 #[cfg(test)]
 mod tests {
-    use crate::{solve, split_even};
+
+    use crate::split_even;
 
     use super::{solve_part_1, solve_part_2};
     const EXAMPLE: &str = include_str!("../example.txt");
     const ACTUAL: &str = include_str!("../input.txt");
+
     #[test]
     fn test_split_half() {
         assert_eq!(None, split_even(123));
         assert_eq!(Some((12, 34)), split_even(1234));
-    }
-    #[test]
-    fn test_zero() {
-        let _guard = tracing::subscriber::set_default(
-            tracing_subscriber::FmtSubscriber::builder()
-                .without_time()
-                .finish(),
-        );
-        let res = solve("0", 16);
-        assert_eq!(res, 0);
     }
 
     #[test]
@@ -120,24 +106,12 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
-    fn test_part2() {
-        let _guard = tracing::subscriber::set_default(
-            tracing_subscriber::FmtSubscriber::builder()
-                .without_time()
-                .finish(),
-        );
-        assert_eq!(format!("{}", solve_part_2(EXAMPLE)), "0");
-    }
-
-    #[test]
-    #[ignore]
     fn test_part2_actual() {
         let _guard = tracing::subscriber::set_default(
             tracing_subscriber::FmtSubscriber::builder()
                 .without_time()
                 .finish(),
         );
-        assert_eq!(format!("{}", solve_part_2(ACTUAL)), "0");
+        assert_eq!(format!("{}", solve_part_2(ACTUAL)), "221632504974231");
     }
 }
