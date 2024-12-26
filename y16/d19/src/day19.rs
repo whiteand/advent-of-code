fd2f54c9ad1abfe041b50922e971ff9a73dd3632
whiteand <andrewbeletskiy@gmail.com>
2024-12-27 00:22:13 +0200

    feat: finished y16 d19 p2

--- y16/d19/src/day19.rs
+++ y16/d19/src/day19.rs
@@ -1,3 +1,5 @@
+use tracing::info;
+
 #[tracing::instrument(skip(file_content))]
 pub fn part1(file_content: &str) -> usize {
     let n = file_content.trim().parse().unwrap();
@@ -9,32 +11,61 @@ fn f(n: usize) -> usize {
     }
     (f(n >> 1) << 1) + ((n & 0b1) << 1) - 1
 }
+
+#[derive(Debug)]
+enum Task {
+    Calc,
+    HandleOdd(usize),
+    HandleEven(usize),
+}
+
 fn g(x: usize) -> usize {
     if x <= 2 {
         return 1;
     }
-    if x & 0b1 == 1 {
-        let n = x >> 1;
-        let n2 = n << 1;
-        let pr = g(n2);
-        if pr < n {
-            pr + 1
-        } else if pr < n2 {
-            pr + 2
-        } else {
-            1
-        }
-    } else {
-        let prev = g(x - 1);
-        let n = x >> 1;
-        if prev < n {
-            prev + 1
-        } else if prev < ((n << 1) - 1) {
-            prev + 2
-        } else {
-            1
+    let mut res = x;
+    let mut tasks = Vec::with_capacity(3014601);
+    tasks.push(Task::Calc);
+    while let Some(task) = tasks.pop() {
+        info!(?res, ?task);
+        match task {
+            Task::Calc => {
+                if res <= 2 {
+                    res = 1;
+                    continue;
+                }
+                if res & 0b1 == 1 {
+                    let n = res >> 1;
+                    tasks.push(Task::HandleOdd(n));
+                    res -= 1;
+                    tasks.push(Task::Calc);
+                } else {
+                    tasks.push(Task::HandleEven(res >> 1));
+                    res -= 1;
+                    tasks.push(Task::Calc);
+                }
+            }
+            Task::HandleOdd(n) => {
+                res = if res < n {
+                    res + 1
+                } else if res < (n * 2) {
+                    res + 2
+                } else {
+                    1
+                };
+            }
+            Task::HandleEven(n) => {
+                res = if res < n {
+                    res + 1
+                } else if res < ((n << 1) - 1) {
+                    res + 2
+                } else {
+                    1
+                };
+            }
         }
     }
+    res
 }
 #[tracing::instrument(skip(file_content))]
 pub fn part2(file_content: &str) -> usize {
@@ -50,7 +81,7 @@ mod tests {
     const ACTUAL: &str = include_str!("../input.txt");
     #[rstest]
     #[case::example(EXAMPLE, "3")]
-    #[case::actual(ACTUAL, "0")]
+    #[case::actual(ACTUAL, "1834903")]
     fn test_part1(#[case] input: &str, #[case] expected: &str) {
         let _guard = tracing::subscriber::set_default(
             tracing_subscriber::FmtSubscriber::builder()
@@ -60,9 +91,8 @@ mod tests {
         assert_eq!(format!("{}", part1(input)), expected);
     }
     #[rstest]
-    #[case::example(EXAMPLE, "2")]
-    #[case::actual(ACTUAL, "0")]
-    #[ignore]
+    #[case::n5("5", "2")]
+    #[case::actual(ACTUAL, "1420280")]
     fn test_part2(#[case] input: &str, #[case] expected: &str) {
         let _guard = tracing::subscriber::set_default(
             tracing_subscriber::FmtSubscriber::builder()
